9ff71702195791be4f3839be34f9d2cc38cd326e
mizuki murata <mizucmke@gmail.com>
2024-11-25 15:21:31 +0900

    課題14git add .viewできたかわかんない！！

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/posts', function (Request $request) {
+    $posts = Post::all();
+    return view('posts.index', ['posts' => $posts]);
+});
